f89fc48905417abf90d6579eb0a374e33d6cef77
Keith Luchtel <keith@keithluchtel.com>
2023-01-31 14:04:03 -0600

    WIP - Initial implementation (#1)
    * WIP
    
    * Add addition config files

---
+++ src/persist.ts
@@ -0,0 +1,52 @@
+import { applySnapshot, IStateTreeNode, onSnapshot } from "mobx-state-tree";
+import {
+  IPersistStoreModel,
+  PeristStoreModel,
+  PersistStoreNode,
+} from "./persistStore";
+
+export type Storage = {
+  getItem: (key: string) => Promise<string>;
+  setItem: (key: string, data: string) => Promise<void>;
+};
+
+export type PersistConfig = {
+  key: string;
+  storage: Storage;
+};
+export type PersistEntry = [IStateTreeNode, PersistConfig];
+
+type PersistProps = Array<PersistEntry>;
+export const persist = (persistEntries: PersistProps) => {
+  const persistStore = PeristStoreModel.create();
+
+  for (const persistEntry of persistEntries) {
+    createPersistNode(persistStore, persistEntry);
+  }
+
+  persistStore.setInitialized(true);
+  return persistStore;
+};
+
+const createPersistNode = async (
+  persistStore: IPersistStoreModel,
+  persistEntry: PersistEntry
+) => {
+  const [node, config] = persistEntry;
+  const persistStoreNode = PersistStoreNode.create({
+    key: config.key,
+  });
+  persistStore.addNode(persistStoreNode);
+
+  // load from storage
+  try {
+    const restoredState = await config.storage.getItem(config.key);
+
+    applySnapshot(node, JSON.parse(restoredState));
+    persistStoreNode.setRehydrated(true);
+  } catch (e) {}
+
+  onSnapshot(node, (snapshot) => {
+    config.storage.setItem(config.key, JSON.stringify(snapshot));
+  });
+};
